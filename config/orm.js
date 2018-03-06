8d95505b506b6106d4a267e1e37fa9c2f45e3ed6
Jerridd <jerridd_speidel@yahoo.com>
2018-03-05 18:21:19 -0600

    Got the handlebars html to run on the localhost

--- config/orm.js
+++ config/orm.js
@@ -1,8 +1,7 @@
 // Import MySQL connection.
 var connection = require("../config/connection.js");
 
-// Helper function for SQL syntax.
-// Let's say we want to pass 3 values into the mySQL query.
+// Helper functio?? // Let's say we want to pass 3 values into the mySQL query.
 // In order to write the query, we need 3 question marks.
 // The above helper function loops through and creates an array of question marks - ["?", "?", "?"] - and turns it into a string.
 // ["?", "?", "?"].toString() => "?,?,?";
@@ -42,7 +41,7 @@ function objToSql(ob) {
 // Object for all our SQL statement functions.
 var orm = {
   all: function(tableInput, cb) {
-    var queryString = "SELECT * FROM " + burgers + ";";
+    var queryString = "SELECT * FROM burgers";
     connection.query(queryString, function(err, result) {
       if (err) {
         throw err;
@@ -51,7 +50,7 @@ var orm = {
     });
   },
   create: function(table, cols, vals, cb) {
-    var queryString = "INSERT INTO " + burgers;
+    var queryString = "INSERT INTO "
 
     queryString += " (";
     queryString += cols.toString();
