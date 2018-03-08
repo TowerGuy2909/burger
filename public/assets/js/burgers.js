f53d3c03f27b9111a9b95ba3245e376afcc58898
Jerridd <jerridd_speidel@yahoo.com>
2018-03-07 22:47:36 -0600

    Got the buttons to add and switch tables to move.

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,20 +1,20 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
-  $(".change-devoured").on("click", function(event) {
+  $(".change-devour").on("click", function(event) {
     var id = $(this).data("id");
-    var newDevoured = $(this).data("newDevoured");
+    var newDevour = $(this).data("newDevour");
 
-    var newDevouredState = {
-      devoured: newDevoured
+    var newDevourState = {
+      devoured: newDevour
     };
 
     // Send the PUT request.
     $.ajax("/api/burgers/" + id, {
       type: "PUT",
-      data: newDevouredState
+      data: newDevourState
     }).then(
       function() {
-        console.log("changed devoured to", newDevoured);
+        console.log("changed devoured to", newDevour);
         // Reload the page to get the updated list
         location.reload();
       }
@@ -26,8 +26,8 @@ $(function() {
     event.preventDefault();
 
     var newBurger = {
-      name: $("#ca").val().trim(),
-      devoured: $("[burger_name=devoured]:checked").val().trim()
+      burger_name: $("#ca").val().trim(),
+      devoured: $("newDevour").val()
     };
 
     // Send the POST request.
@@ -47,14 +47,14 @@ $(function() {
     var id = $(this).data("id");
 
     // Send the DELETE request.
-    // $.ajax("/api/burgers/" + id, {
-    //   type: "DELETE",
-    // }).then(
-    //   function() {
-    //     console.log("deleted burger", id);
-    //     // Reload the page to get the updated list
-    //     location.reload();
-    //   }
-    // );
+    $.ajax("/api/burgers/" + id, {
+      type: "DELETE",
+    }).then(
+      function() {
+        console.log("deleted burger", id);
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
   });
 });
