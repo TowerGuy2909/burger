75800b73a68cefaa9ad62acdc8f07e84148d63f0
Jerridd <jerridd_speidel@yahoo.com>
2018-03-10 13:10:49 -0600

    Attempt 3 heroku post

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
-var dotenv = require("dotenv")
+require("dotenv").config();
 var connection;
 
 if (process.env.JAWSDB_URL) {
@@ -10,7 +10,7 @@ if (process.env.JAWSDB_URL) {
     port: 3306,
     host: "localhost",
     user: "root",
-    password: "process.env.PASSWORD",
+    password: process.env.PASSWORD,
     database: "burgers_db"
   });
 };
